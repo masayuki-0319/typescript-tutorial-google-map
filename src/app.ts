2d108f4a9b405be9a9225f59051d2290b1a1de8d
将之大西 <omccello@gmail.com>
2021-06-30 00:06:25 +0900

    step173

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 require('dotenv').config();
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY as string;
 console.log('GOOGLE_API_KEY', GOOGLE_API_KEY);
@@ -5,10 +7,39 @@ console.log('GOOGLE_API_KEY', GOOGLE_API_KEY);
 const form = document.querySelector('form');
 const addressInput = document.getElementById('address') as HTMLInputElement;
 
+type GoogleGeoCodingResponse = {
+  status: 'OK';
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+};
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
 
-  const enteredAddrss = addressInput;
+  const enteredAddrss = encodeURI(addressInput.value);
+
+  axios
+    .get<GoogleGeoCodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${enteredAddrss}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== 'OK') {
+        throw new Error('座標を取得できませんでした。');
+      }
+
+      const coordinates = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById('map') as HTMLInputElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.error(err);
+    });
 };
 
 form?.addEventListener('submit', searchAddressHandler);
