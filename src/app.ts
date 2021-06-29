9d08a864abc936736d2fb6b375b48e6fd788bd18
将之大西 <omccello@gmail.com>
2021-06-29 23:08:01 +0900

    step170

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// ここにコードを書きます
+const form = document.querySelector('form');
+const addressInput = document.getElementById('address') as HTMLInputElement;
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault();
+
+  const enteredAddrss = addressInput;
+};
+
+form?.addEventListener('submit', searchAddressHandler);
